be56b1f49d50a08f396a64e7965b0b9f9c83ec13
gunjan <gunjan.kumari@sukriti.ngo>
2023-08-24 17:26:21 +0530

    home

--- src/Desk/Navbar/Navbar.jsx
+++ src/Desk/Navbar/Navbar.jsx
@@ -1,30 +1,39 @@
-import React from 'react'
-import logo from "../Navbar/group-3.svg"
-import {Link} from  "react-router-dom"
-import './Navbar.css'
-
+import React from "react";
+import logo from "../Navbar/group-3.svg";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <>
-    <div className='container-of-navigation-and-logo' >
-         <div className='container-of-logo'>
-             <img src= {logo} /> 
-           </div>
-          
-      <div className='container-of-navigation-bar'>
+      <div className="container-of-navigation-and-logo">
+        <div className="container-of-logo">
+          <img className="logo" src={logo} alt="" />
+        </div>
 
-         
-          <Link to="/" className='container-of-nav-text'>HOME</Link>
-          <Link to="/about" className='container-of-nav-text'>ABOUT</Link>
-          <Link to="/ecomitra" className='container-of-nav-text'>ECOMITRA</Link>
-          <Link to="/rewater" className='container-of-nav-text'>REWATER</Link>
-          <Link to="/careers" className='container-of-nav-text'>CAREERS</Link>
-          <Link to="/contact" className='container-of-nav-text'>CONTACT US</Link>
-      </div>
+        <div className="container-of-navigation-bar">
+          <Link to="/" className="container-of-nav-text">
+            HOME
+          </Link>
+          <Link to="/about" className="container-of-nav-text">
+            ABOUT
+          </Link>
+          <Link to="/ecomitra" className="container-of-nav-text">
+            ECOMITRA
+          </Link>
+          <Link to="/rewater" className="container-of-nav-text">
+            REWATER
+          </Link>
+          <Link to="/careers" className="container-of-nav-text">
+            CAREERS
+          </Link>
+          <Link to="/contact" className="container-of-nav-text">
+            CONTACT US
+          </Link>
+        </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
