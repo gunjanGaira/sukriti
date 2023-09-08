6cfcf616e04d5bccaaca47024468bc653806d88a
gunjan <gunjan.kumari@sukriti.ngo>
2023-09-08 15:02:59 +0530

    ecomitra

--- src/Desk/Ecomitra/Ecomitra.jsx
+++ src/Desk/Ecomitra/Ecomitra.jsx
@@ -6,6 +6,16 @@ import Group1 from "../Ecomitra/group1.jpg";
 import Group2 from "../Ecomitra/group2.jpg";
 import Group3 from "../Ecomitra/group3.jpg";
 import Group4 from "../Ecomitra/group4.jpg";
+import Factor1 from "../Ecomitra/keyOne.png";
+import Factor2 from "../Ecomitra/keyTwo.png";
+import Factor3 from "../Ecomitra/keyThree.png";
+import smartHygieneIcon from "../Ecomitra/smart-hygiene-icon.svg";
+import smartentryIcon from "../Ecomitra/smart-entry-icon.svg";
+import wasteWaterIcon from "../Ecomitra/water-treatment-icon.png";
+import healthMonitoring from "../Ecomitra/smart-health-icon.svg";
+import solarEnergy from "../Ecomitra/solar-energy.svg";
+import robustStructure from "../Ecomitra/Robust-Structure.svg";
+
 
 const Ecomitra = () => {
   return (
@@ -59,21 +69,21 @@ const Ecomitra = () => {
             <div>
               <img
                 data-u="image"
-                src={Group3}
+                // src={Group3}
                 style={{ paddingRight: "20px" }}
               />
             </div>
             <div>
               <img
                 data-u="image"
-                src={Group4}
+                // src={Group4}
                 style={{ paddingRight: "20px" }}
               />
             </div>
             <div>
               <img
                 data-u="image"
-                src={Group2}
+                // src={Group2}
                 style={{ paddingRight: "20px" }}
               />
             </div>
@@ -118,67 +128,132 @@ const Ecomitra = () => {
           </div>
         </div>
       </div>
-     
-     {/* section three */}
-      <div>
+
+      {/* section three */}
+      <div className="challenges_container">
         <div>
-          <p>
+          <p className="headingOfChallenges">
             Today, public toilets in India suffer from three major challenges:
           </p>
         </div>
-        <div>
-          <div>
-            <div>
-              <div>
-                <div>
-                  <img src="./assets/home/rectangle-14-copy-5@3x.png" />
-                </div>
-                <div>
-                  <p>Constant maintenance</p>
-                </div>
-              </div>
-              <div>
-                <div>
-                  <img src="./assets/home/rectangle-14-copy-3@3x.png" />
-                </div>
-                <div>
-                  <p>
-                    Uninterrupted water supply to meet high water requirements
-                  </p>
-                </div>
-              </div>
-              <div>
-                <div>
-                  <img
-                    class="image small-icon"
-                    src="./assets/home/rectangle-14-copy-6@3x.png"
-                  />
-                </div>
-                <div>
-                  <p>Inadequate waste disposal mechanisms</p>
-                </div>
-              </div>
-            </div>
+
+        <div className="keys_con">
+          <div className="key1">
+            <img src={Factor1} className="fectorImg" />
+
+            <p className="peraKey">Constant maintenance</p>
+          </div>
+
+          <div className="key2">
+            <img src={Factor2} className="fectorImg" />
+            <p className="peraKey">
+              Uninterrupted water supply to meet <br /> high water requirements
+            </p>
+          </div>
+
+          <div className="key3">
+            <img src={Factor3} className="fectorImg" />
+            <p className="peraKey">
+              Inadequate waste disposal <br /> mechanisms
+            </p>
           </div>
         </div>
 
-        <div>
-          <div>
-            <div>
-              <div>
-                <p>
-                  A combination of these three factors forces the general
-                  public, especially women to use open spaces for urination and
-                  defecation. This not only compromises their dignity and
-                  safety, but also their health, causing various diseases,
-                  chronic illness, malnutrition, and on a much larger scale; the
-                  loss of human resource!
-                </p>
-              </div>
-            </div>
-          </div>
+        <div className="keyPera">
+          <p
+            className="LONG-PERA"
+            style={{
+              fontFamily: "'Segoe UI', Tahoma, Geneva, Verdana, sans-serif ",
+              lineHeight: "1.6",
+            }}
+          >
+            A combination of these three factors forces the general public,
+            especially women to use open spaces for urination and defecation.
+            This not
+            <br /> only compromises their dignity and safety, but also their
+            health, causing various diseases, chronic illness, malnutrition, and
+            on a much larger <br />
+            scale; the loss of human resource!
+          </p>
         </div>
       </div>
+      {/* section four */}
+
+      <div>
+
+        <div>
+          <p>ECOMITRA </p>
+          <span>
+            <span>:</span> A SMART SOLUTION
+          </span>
+          <hr />
+        </div>
+
+        <div className="solution-Container">
+            <div style={{display:"flex"}} >
+              <img src={smartHygieneIcon} />
+              <p>
+                <span>Smart hygiene maintenance: </span>
+                eliminates manual cleaning with auto<br/> flushing and floor
+                cleaning. It also decides the optimum water required.
+              </p>
+          </div>
+
+            <div style={{display:"flex"}}>
+              <img src={smartentryIcon} />
+             <p>
+                <span>Smart entry management:</span>
+                stops user entry in case of empty water <br/> tanks or chokes, a
+                patented sensorless tech. <br />
+              </p>
+          </div>
+
+          <div style={{display:"flex"}}>
+              <img src={wasteWaterIcon}/>
+              <p>
+                <span>On-site waste water treatment:</span>
+                saves 80% water by recycling it for <br/> flushing, floor cleaning,
+                etc. A patented model, it is maintenance free <br/> with zero
+                operational costs.
+                <br />
+              </p>
+          </div>
+
+          <div style={{display:"flex"}}>
+              <img src={healthMonitoring} />
+             <p>
+                <span>Smart health monitoring:</span>
+                conveys maintenance schedule to central <br/> monitoring station based
+                on the toilet’s health.
+                <br />
+              </p>
+          </div>
+
+
+          <div style={{display:"flex"}}>
+              <img src={solarEnergy}/>
+              <p>
+                <span>Runs on solar energy:</span> no grid connection needed.
+                <br />
+              </p>
+          </div>
+
+          <div style={{display:"flex"}}>
+              <img src={robustStructure} />
+              <p>
+                <span>Robust Structure:</span>
+                Our toilet is a strong and robust RCC superstructure.<br/> The use of
+                metallic fixtures and sturdy fittings ensure their use over a<br/>
+                long period of time and reduce the probability and need of <br/>
+                replacement.
+                <br />
+              </p>
+          </div>
+
+
+        </div>
+      </div>
+
     </div>
   );
 };
