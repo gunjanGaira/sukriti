6f30d3524a6e593aafc6cfc8baaf0d89c0f2dbd3
gunjan <gunjan.kumari@sukriti.ngo>
2023-09-12 19:01:18 +0530

    rewater

--- src/Desk/Rewater/Rewater.jsx
+++ src/Desk/Rewater/Rewater.jsx
@@ -10,11 +10,21 @@ import automated from "../Rewater/Fully-automated-icon.svg";
 import HybridTechnology from "../Rewater/hybrid-technology-icon.svg";
 import BlackWater from "../Rewater/black-water.svg";
 import SolarEnergy from "../Rewater/solar-energy.svg";
-import Shipra_River from "../Rewater/10.png";
+import Shipra_River from "../Rewater/10.JPG";
 import Rewater_video from "../Rewater/rewater_vid.mp4";
 import PLAY_icon from "../Rewater/play.png";
 
 const Rewater = () => {
+  const my_func=()=>{
+    if(document.getElementById('player').paused){
+      document.getElementById('player').play()
+      ('#play_btn').hide();
+    }
+    else{
+      document.getElementById('player').pause()
+      ('#play_btn').show();
+    }
+  };
   return (
     <div>
       {/* section one */}
@@ -86,72 +96,96 @@ const Rewater = () => {
           </p>
           {/* <hr class="el_hr" /> */}
         </div>
-         <div className="content_img_con">
-        <div className="inner-regenrating-container">
-          <div className="icon-content">
-            <img src={prefabricated} />
-            <p className="rewater_pera">
-              <span style={{fontWeight:"650"}}>Prefabricated waste treatment unit:</span> simple and quick
-              underground installation.
-            </p>
+        <div className="content_img_con">
+          <div className="inner-regenrating-container">
+            <div className="icon-content">
+              <img src={prefabricated} />
+              <p className="rewater_pera">
+                <span style={{ fontWeight: "650" }}>
+                  Prefabricated waste treatment unit:
+                </span>{" "}
+                simple and quick underground installation.
+              </p>
+            </div>
+
+            <div className="icon-content">
+              <img src={automated} />
+
+              <p className="rewater_pera">
+                <span style={{ fontWeight: "650" }}>Fully automated:</span>
+                self-functioning unit that requires no manual intervention.
+              </p>
+            </div>
+
+            <div className="icon-content">
+              <img src={HybridTechnology} />
+
+              <p className="rewater_pera">
+                <span style={{ fontWeight: "650" }}>
+                  Uses hybrid technology:
+                </span>
+                produces minimum amount of foul gases.
+                <br />
+              </p>
+            </div>
+
+            <div className="icon-content">
+              <img src={BlackWater} />
+
+              <p className="rewater_pera">
+                <span style={{ fontWeight: "650" }}>Recycles black water:</span>
+                safe for non-potable use such as washing, flushing, cooling,
+                horticulture, etc.
+                <br />
+              </p>
+            </div>
+
+            <div className="icon-content">
+              <img src={SolarEnergy} />
+
+              <p className="rewater_pera">
+                <span style={{ fontWeight: "650" }}>Runs on solar energy:</span>
+                no grid connection needed.
+                <br />
+              </p>
+            </div>
+
+            <div className="icon-content">
+              <img src={SolarEnergy} />
+
+              <p className="rewater_pera">
+                <span style={{ fontWeight: "650" }}>
+                  Zero operational costs.
+                </span>
+                <br />
+              </p>
+            </div>
+          </div>
+          <div className="vidImage" onClick={my_func}>
+            <video
+              controls
+              id="player"
+              poster={Shipra_River}
+              className="rivar-imag"
+            >
+              <source src={Rewater_video} type="video/mp4" />
+            </video>
+            <img id="play_btn" src={PLAY_icon} className="play_ic" />
+          </div>
+          {/* part two */}
+          <div className="vid-Image" onClick={my_func}>
+            <video
+              controls
+              id="player"
+              poster={Shipra_River}
+              className="rivar_Image"
+            >
+              <source src={Rewater_video} type="video/mp4" />
+            </video>
+            <img id="play_btn" src={PLAY_icon} className="play_ic" />
           </div>
 
-          <div className="icon-content">
-            <img src={automated} />
-
-            <p className="rewater_pera">
-              <span style={{fontWeight:"650"}}>Fully automated:</span>
-              self-functioning unit that requires no manual intervention.
-            </p>
-          </div>
-
-          <div className="icon-content">
-            <img src={HybridTechnology} />
-
-            <p className="rewater_pera">
-              <span style={{fontWeight:"650"}}>Uses hybrid technology:</span>
-              produces minimum amount of foul gases.
-              <br />
-            </p>
-          </div>
-
-          <div className="icon-content">
-            <img src={BlackWater} />
-
-            <p className="rewater_pera">
-              <span style={{fontWeight:"650"}}>Recycles black water:</span>
-              safe for non-potable use such as washing, flushing, cooling,
-              horticulture, etc.
-              <br />
-            </p>
-          </div>
-
-          <div className="icon-content">
-            <img src={SolarEnergy} />
-
-            <p className="rewater_pera">
-              <span style={{fontWeight:"650"}}>Runs on solar energy:</span>
-              no grid connection needed.
-              <br />
-            </p>
-          </div>
-
-          <div className="icon-content">
-            <img src={SolarEnergy} />
-
-            <p className="rewater_pera">
-              <span style={{fontWeight:"650"}}>Zero operational costs.</span>
-              <br />
-            </p>
-          </div>
         </div>
-        <div className="vidImage">
-      <video poster={Shipra_River} className="rivar-imag">
-          <source src={Rewater_video} type="video/mp4"/>
-      </video>
-        <img src={PLAY_icon} className="play_ic"/>
-      </div>
-      </div>
       </div>
     </div>
   );
